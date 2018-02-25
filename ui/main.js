064e2231d9ee76bc526468a9966288506f1a4686
tomboy09 <36487481+tomboy09@users.noreply.github.com>
2018-02-25 19:52:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,12 +5,12 @@ console.log('Loaded!');
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-  marginLeft=marginLeft+10;
+  marginLeft=marginLeft+5;
   img.style.marginLeft=marginLeft+'px';
   
   
 }
 img.onclick=function(){
-    var interval=setInterval(moveRight,100);
+    var interval=setInterval(moveRight,50);
     
 };
