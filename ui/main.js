db36fa77e20641ae031026f092c2ef8ff64e74a3
tomboy09 <36487481+tomboy09@users.noreply.github.com>
2018-02-26 02:14:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,7 @@ button.onclick=function(){
             //take some action
             if(request.status===200){
                var counter= request.responseText;
-                var span=document.getElementById('count');
+                var span=document.getElementById("count");
                 span.innerHTML=counter.toString();
             }
         }
