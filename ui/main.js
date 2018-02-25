92d11289222bab01d4e61b0581c50e2a6d1fb774
tomboy09 <36487481+tomboy09@users.noreply.github.com>
2018-02-26 01:09:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,3 +14,21 @@ img.onclick=function(){
     var interval=setInterval(moveRight,50);
     
 };
+//move image
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function(){
+    //make a request to the counter end point 
+    var request=new XMLhttpRequest();
+    
+    
+    
+    //capture the response and store it in a variable
+    
+    
+    //render the varible in the correct span
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHtml=counter.toString();
+};
+
